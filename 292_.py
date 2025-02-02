f4e7a3d172f7f3cf65cd23bcdbc31c6f92029bcd
Veysel Tandoğru <112335674+VeyselTandogru@users.noreply.github.com>
2025-02-02 14:37:19 +0300

    Delete 292_.py

--- 292_.py
+++
@@ -1,7 +0,0 @@
-# 292. Nim Game
-# Bu oyunu kazanıp kazanamayacağını belirleyen şey taş sayısının 4’e bölümünden kalanı.
-# 4'e bölümünden kalan 0 ise kaybetmek zorundayız.
-# 4'e bölümünden kalan 0 değilse kazanabilir.
-class Solution:
-    def canWinNim(self, n: int) -> bool:
-        return True if n % 4 != 0 else False  
